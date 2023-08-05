8ddf042421fbcb78cff4ece14434fae59ae987b1
Leonardo Lezcano <leonardo.d.lezcano@gmail.com>
2023-08-05 17:12:44 -0300

    Navbar seccion completado

---
+++ main.js
@@ -0,0 +1,15 @@
+// Drop Login
+let loginBtn = document.querySelector(".login-btn");
+let dropLogin = document.querySelector(".drop-login");
+
+loginBtn.onclick = () => {
+  dropLogin.classList.toggle("drop-login-open");
+};
+
+// Menu Open Close
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("move");
+};
