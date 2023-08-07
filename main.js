afec1a50d02e74c386d57babb2ce926a05c7bf3b
Leonardo Lezcano <leonardo.d.lezcano@gmail.com>
2023-08-07 00:57:42 -0300

    Home section completo

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ let navbar = document.querySelector(".navbar");
 menu.onclick = () => {
   menu.classList.toggle("move");
 };
+
+//Home Swiper
+var swiper = new Swiper(".home", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 25000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
